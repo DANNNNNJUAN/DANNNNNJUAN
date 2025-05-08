4c29dc962506cdd3c51cb959652eba859940658b
DANNNNNJUAN <dannnnnjuan@outlook.com>
2025-05-08 22:44:13 +0800

    add test

---
+++ leetcode/mergelist.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        sorted = []
+        p1, p2 = 0, 0  
+        while p1 < (m - n) or p2 < n:
+            
+            if p1 == (m - n):
+                sorted.append(nums2[p2])
+                p2 += 1
+            elif p2 == n:
+                sorted.append(nums1[p1])
+                p1 += 1
+            elif nums1[p1] < nums2[p2]:
+                sorted.append(nums1[p1])
+                p1 += 1
+            elif nums1[p1] > nums2[p2]:
+                sorted.append(nums2[p2])
+                p2 += 1
+            #index out of range
+            
+        nums1 = sorted
+        return nums1
+        
